78de9518edb124a8ca1a9062eb4099ab43714970
Kazuichiro Taira <ktaira098@gmail.com>
2019-02-19 20:18:44 +0200

    Third lesson

--- feb19.py
+++ feb19.py
@@ -1,19 +1,36 @@
-def is_tasty(fish, tasty):
-
-    if fish is tasty:
-        print(f"{fish} is good")
-
-    else:
-        print(f"{fish} is a nightmare")
+# def is_tasty(fish, tasty):
+#
+#     if tasty:
+#         print(f"{fish} is good")
+#
+#     else:
+#         print(f"{fish} is a nightmare")
 
 
 def is_tasty_according_to_me(fish):
 
-    if fish in ["salmon", "tuna"]:
+    # if fish in ["salmon", "tuna"]:
+    #
+    #     print(f"{fish} is good")
+    # comment above is same as below.
+
+    good_fishes = ["salmon", "tuna", "fugu"]
+
+    if fish in good_fishes:
         print(f"{fish} is good")
 
     else:
         print(f"{fish} is a nightmare")
+
+
+def picky(fish):
+
+    if fish == "salmon":
+
+        print(f"{fish} is good")
+
+    else:
+        print("I want salmon!!")
 
 
 if __name__ == "__main__":
@@ -27,4 +44,16 @@ if __name__ == "__main__":
     # tasty = (1, 2)  # Tuple
 
     # is_tasty("cat fish", False)
-    is_tasty_according_to_me("tuna")
+    # is_tasty_according_to_me("tuna")
+
+    kyounosakana = ["shark", "fugu", "uni", "tako", "ikura", "irabuchar", "tuna"]
+
+    # for shiro in kyounosakana:
+    #
+    #     is_tasty_according_to_me(shiro)
+    #
+    for guu in kyounosakana:
+
+        print(guu)
+
+        picky(guu)
