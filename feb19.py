f065ac6d338caf0f016564ae0291a08acd5c5dd7
Aurélien Nioche <nioche.aurelien@gmail.com>
2019-02-19 19:20:32 +0200

    third lesson

--- feb19.py
+++ feb19.py
@@ -1,5 +1,30 @@
-def salmon fish
+def is_tasty(fish, tasty):
 
-    if fish is salmon
+    if fish is tasty:
+        print(f"{fish} is good")
 
-        print salmon is good
+    else:
+        print(f"{fish} is a nightmare")
+
+
+def is_tasty_according_to_me(fish):
+
+    if fish in ["salmon", "tuna"]:
+        print(f"{fish} is good")
+
+    else:
+        print(f"{fish} is a nightmare")
+
+
+if __name__ == "__main__":
+
+    # # The different types of variable
+    # tasty = 3  # Integer
+    # tasty = 3.4 # Float
+    # tasty = "3" # String
+    # tasty = True # Boolean
+    # tasty = [1, 2] # List
+    # tasty = (1, 2)  # Tuple
+
+    # is_tasty("cat fish", False)
+    is_tasty_according_to_me("tuna")
