02337d1d93dfff13ddc58b7946528bdf6b6c2890
Kazuichiro Taira <ktaira098@gmail.com>
2019-03-29 16:54:42 +0200

    turtle move

--- turtle318.py
+++ turtle318.py
@@ -2,20 +2,50 @@ import turtle
 
 kaz = turtle.Turtle()
 
-kaz.color('blue')
-kaz.pensize(6)
+kaz.color("blue", "cyan")
+#   (#3cc9118) Hex Value or RGB Value
+
+kaz.pensize(1)
+
 kaz.shape('turtle')
+
+# kaz.forward(100)
+# kaz.left(90)
+# kaz.penup()
+# kaz.forward(100)
+# kaz.right(90)
+# kaz.pendown()
+#
+# kaz.forward(100)
+
+kaz.begin_fill()
+
 
 kaz.forward(100)
 kaz.left(90)
+kaz.forward(100)
+kaz.left(90)
+kaz.forward(100)
+kaz.left(90)
+kaz.forward(100)
 
 kaz.penup()
-kaz.forward(100)
-kaz.right(90)
+kaz.forward(150)
 kaz.pendown()
 
 kaz.forward(100)
+kaz.left(90)
+kaz.forward(100)
+kaz.left(90)
+kaz.forward(100)
+kaz.left(90)
+kaz.forward(100)
 
 
+
+
+
+
+kaz.end_fill()
 
 turtle.done()
