f0a323593a30bce55322f72aa89a38d191eda0c9
Kazuichiro Taira <ktaira098@gmail.com>
2019-04-26 13:33:49 +0300

    new

---
+++ pi.py
@@ -0,0 +1,17 @@
+import gpiozero
+import time
+
+led = gpiozero.LED(2)
+
+white True:
+        led.on()
+        time.sleep(1)
+        led.off()
+        time.sleep(1)
+
+
+import gpiozero
+import time
+import signal
+
+
